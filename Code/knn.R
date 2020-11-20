aac66c3c50453a8b4a958469614b22a4e17604a5
David Reynolds <davidreynolds@mail.missouri.edu>
2020-11-20 12:42:46 -0600

    added text for knn and nn

--- Code/knn.R
+++ Code/knn.R
@@ -6,63 +6,85 @@ library(caret)
 library(class)
 library(data.table)
 library(dplyr)
+library(ggplot2)
 library(mltools)
 
 # Read in the data
 setwd("~/Downloads/Documents/GitHub/Data-III-Project-2/Data")
-x <- read.table("Xtrain.txt")
-y <- read.table("Ytrain.txt")
-y <- rename(y, author = V1)
-y$author <- as.factor(y$author)
-train <- cbind(x, y)
-test <- read.table("Xtest.txt")
+x_train <- read.table("Xtrain.txt")
+y_train <- read.table("Ytrain.txt")
+y_train <- rename(y_train, author = V1)
+y_train$author <- as.factor(y_train$author)
+train <- cbind(x_train, y_train)
+x_test <- read.table("Xtest.txt")
 y_hot <- one_hot(as.data.table(train$author))
 
 ## KNN
 
-# Implement 10-fold cross-validation
+# Train the classifier and estimate the test error using 10-fold cross-validation
 train_control <- trainControl(method = "cv", number = 10)
 
-# Train the classifier
-set.seed(1)
-knn <- caret::train(author ~., data = train, method = "knn", trControl = train_control, 
-             preProcess = c("center", "scale"), tuneLength = 20)
-knn
-
-# Plot of number of neighbors vs. accuracy
-plot(knn)
-
-# Report the confusion matrix and accuracy (0.7316)
-confusionMatrix(knn)
-
-# Calculate the training cross-entropy (3757.638)
-knn_pred <- predict(knn, newdata = train, type = "prob")
-knn_pred <- as.matrix(knn_pred)
-knn_ce <- -sum(colSums(y_hot*log(knn_pred + 1e-15)))
-
-# Try smaller values of k
-set.seed(2)
-knn_errors = rep(NA, 4)
-for (i in 1:4) {
+knn_accuracy = rep(NA, 15)
+for (i in 1:15) {
   knn = caret::train(author ~., data = train, method = "knn", trControl = train_control, 
-              preProcess = c("center", "scale"), tuneGrid = data.frame(k = i))
-  knn_errors[i] = as.numeric(knn$results[2])
+                     preProcess = c("center", "scale"), tuneGrid = data.frame(k = i))
+  knn_accuracy[i] = as.numeric(knn$results[2])
 }
 
-# Train the classifier using k = 2
-set.seed(3)
-knn <- caret::train(author ~., data = train, method = "knn", trControl = train_control, 
-                    preProcess = c("center", "scale"), tuneGrid = data.frame(k = 2))
+# Calculate the training cross-entropy for range of k
+set.seed(2)
+knn_ce_train = rep(NA, 15)
+for (i in 1:15) {
+  knn = caret::train(author ~., data = train, method = "knn", preProcess = c("center", "scale"), 
+                     tuneGrid = data.frame(k = i))
+  knn_pred = predict(knn, newdata = train, type = "prob")
+  knn_pred = as.matrix(knn_pred)
+  knn_ce_train[i] = -sum(colSums(y_hot*log(knn_pred + 1e-15)))
+}
 
-# Report the confusion matrix and accuracy (0.727)
+# Train the classifier and estimate the "test CE" using 5-fold cross-validation
+K <- 5
+I <- 15
+set.seed(3)
+folds <- sample(1:K, nrow(train), replace = TRUE)
+knn_ce_test <- matrix(data = NA, nrow = K, ncol = I)
+
+for (k in 1:K) {
+  cv_train = train[folds != k, ]
+  cv_test = train[folds == k, ]
+  y_hot = one_hot(as.data.table(cv_test$author))
+  for (i in 1:15) {
+    knn = caret::train(author ~., data = cv_train, method = "knn", preProcess = c("center", "scale"), 
+                       tuneGrid = data.frame(k = i))
+    knn_pred = predict(knn, newdata = cv_test, type = "prob")
+    knn_pred = as.matrix(knn_pred)
+    knn_ce_test[k,i] = -sum(colSums(y_hot*log(knn_pred + 1e-15)))
+  }
+  knn_ce_cv <<- colMeans(knn_ce_test)
+}
+
+# Plot the number of neighbors vs. cross-validated CE
+k_ce <- data.frame(ce = knn_ce_cv)
+k_ce$k <- rownames(k_ce)
+k_ce$k <- as.numeric(k_ce$k)
+
+ggplot(data = k_ce, aes(x = k, y = ce, group = 1)) + geom_line() + xlab("K") + ylab("Cross-validated CE") + 
+  ggtitle("Number of neighbors vs. cross-validated CE") + theme_minimal()
+
+# Train the best classifier on the full training set
+knn <- caret::train(author ~., data = train, method = "knn", preProcess = c("center", "scale"),
+                    tuneGrid = data.frame(k = 15))
+
+# Report the confusion matrix and accuracy (0.6704)
 confusionMatrix(knn)
 
-# Calculate the training cross-entropy (1590.08)
+# Calculate the training cross-entropy (6223.914)
+y_hot <- one_hot(as.data.table(train$author))
 knn_pred <- predict(knn, newdata = train, type = "prob")
 knn_pred <- as.matrix(knn_pred)
 knn_ce <- -sum(colSums(y_hot*log(knn_pred + 1e-15)))
 
 # Save the test prediction probabilities
-Ppred4 <- predict(knn, newdata = test, type = "prob")
+Ppred4 <- predict(knn, newdata = x_test, type = "prob")
 Ppred4 <- as.matrix(Ppred4)
 save(Ppred4, file = "Ppred4.RData")
--- Code/nn.R
+++ Code/nn.R
@@ -15,7 +15,6 @@ y_train <- read.table("Ytrain.txt")
 y_train <- rename(y_train, author = V1)
 y_train$author <- as.factor(y_train$author)
 x_test <- read.table("Xtest.txt")
-y_hot <- one_hot(as.data.table(y_train$author))
 
 # Scale the predictors
 rescale <- function(x, x_min, x_max) {
@@ -28,21 +27,51 @@ x_min <- apply(x_train, 2, min)
 x_max <- apply(x_train, 2, max)
 x_train_scaled <- t(apply(x_train, 1, rescale, x_min, x_max))
 x_test_scaled <- t(apply(x_test, 1, rescale, x_min, x_max))
+train <- cbind(x_train_scaled, y_train)
 
-# Train the neural network
+# Train the neural network and estimate the "test CE" using 5-fold cross-validation (1996.641)
+K <- 5
 set.seed(1)
-nn <- neuralnetwork(X = x_train_scaled, y = y_train, hidden.layers = c(9, 8, 7), optim.type = "adam",
-                    n.epochs = 10, activ.functions = "tanh", learn.rates = 0.01, val.prop = 0.05)
+folds <- sample(1:K, nrow(train), replace = TRUE)
+nn_ce <- rep(NA, K)
+
+for (k in 1:K) {
+  cv_train = train[folds != k, ]
+  cv_test = train[folds == k, ]
+  x_cv_train = cv_train[, 1:10]
+  y_cv_train = cv_train[, 11]
+  x_cv_test = cv_test[, 1:10]
+  y_cv_test = cv_test[, 11]
+  y_hot = one_hot(as.data.table(y_cv_test))
+  set.seed(2)
+  nn = neuralnetwork(X = x_cv_train, y = y_cv_train, hidden.layers = c(9, 8, 7), optim.type = "adam",
+                     n.epochs = 10, activ.functions = "tanh", learn.rates = 0.05, 
+                     val.prop = 0.05, verbose = FALSE)
+  nn_pred = predict(nn, newdata = x_cv_test, probability = TRUE)
+  nn_probs = as.data.frame(nn_pred$probabilities)
+  nn_probs = nn_probs %>% 
+    select("1" = class_1, "4" = class_4, "5" = class_5, "6" = class_6, "7" = class_7, "8" = class_8,
+           "9" = class_9, "11" = class_11, "12" = class_12)
+  nn_probs <- as.matrix(nn_probs)
+  nn_ce[k] <- -sum(colSums(y_hot*log(nn_probs + 1e-15)))
+}
+mean(nn_ce)
+
+# Train the best neural network on the full training set
+set.seed(3)
+nn <- neuralnetwork(X = x_train_scaled, y = y_train, hidden.layers = c(9, 8, 7), optim.type = "adam", 
+                    n.epochs = 10, activ.functions = "tanh", learn.rates = 0.05, val.prop = 0.05, 
+                    verbose = FALSE)
 
 # Plot the loss during training
 plot(nn)
 
-# Report the confusion matrix and accuracy (0.2003)
+# Report the confusion matrix and accuracy (0.2272)
 nn_pred <- predict(nn, newdata = x_train_scaled)
 nn_table <- table(truth = y_train$author, fitted = nn_pred$predictions)
 sum(diag(nn_table))/length(y_train$author)
 
-# Calculate the training cross-entropy (7164.495)
+# Calculate the training cross-entropy (10049.33)
 nn_pred <- predict(nn, newdata = x_train_scaled, probability = TRUE)
 nn_probs <- as.data.frame(nn_pred$probabilities)
 
@@ -51,6 +80,7 @@ nn_probs <- nn_probs %>%
          "9" = class_9, "11" = class_11, "12" = class_12)
 
 nn_probs <- as.matrix(nn_probs)
+y_hot = one_hot(as.data.table(y_train$author))
 nn_ce <- -sum(colSums(y_hot*log(nn_probs + 1e-15)))
 
 # Save the test prediction probabilities
